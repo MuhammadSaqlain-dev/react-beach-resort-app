798dcae87748f5fa5f51a7e2c680bfc1af9c7a88
saqla <2mesaqlain@gmail.com>
2022-05-22 22:08:14 +0500

    InitialCommit🎈

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { RoomProvider } from "./context";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import NotFound from "./pages/NotFound";
+import SingleRoom from "./pages/SingleRoom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RoomProvider>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/rooms" element={<Rooms />} />
+        <Route exact path="/rooms/:slug" element={<SingleRoom />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </RoomProvider>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+
+class NavBar extends Component {
+  state = { isOpenMenu: false };
+
+  handleNavIcon = () => this.setState({ isOpenMenu: !this.state.isOpenMenu });
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="nav-center">
+            <div className="nav-header">
+              <Link to={{ pathname: "/" }}>
+                <img src={logo} alt="Hotel Resort" />
+              </Link>
+              <button
+                className="nav-btn"
+                type="button"
+                onClick={this.handleNavIcon}
+              >
+                <FaAlignRight className="nav-icon" />
+              </button>
+            </div>
+            <ul
+              className={
+                this.state.isOpenMenu ? "nav-links show-nav" : "nav-links"
+              }
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/rooms">Rooms</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default NavBar;
