2461b7ef35c37fe9155b4ddc412625239d59f25d
saqla <2mesaqlain@gmail.com>
2022-05-22 22:13:27 +0500

    InitialCommit🎈

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "wtma1vhii2ij",
-  accessToken: "9vhwnItgbcCXnWS62O8K9Na8DK1QQ7PQmT6nc9yeWyo",
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
